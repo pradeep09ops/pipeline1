2a0f1a2cc679b078f49126b6097e7531a99f2420
pradeep09ops <56946898+pradeep09ops@users.noreply.github.com>
2024-06-13 20:23:37 +0530

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,2 +1,6 @@
 ﻿write-host "Hello World, this is Pradeep."
+$data1 = #{ServiceName}#
+$data2 = #{ConnectionString}#
 
+write-host "Service Name is: $data1"
+write-host "Connection String for db: $data2"
